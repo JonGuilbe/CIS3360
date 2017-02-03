886d99346a5009f52dc810141ebc022147cde032
JonGuilbe <jguilbe@knights.ucf.edu>
2017-02-03 09:47:08 -0500

    Test2

--- hillcipher.java
+++ hillcipher.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class hillcipher{
     public static void main(String[] args){
         Scanner stdin = new Scanner(System.in);
+        String encriptionKeyFile = args[0];
+        String fileToEncript = args[1];
     }
 }
